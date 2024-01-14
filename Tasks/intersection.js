cf921103367faf807242a8c8b6343dcaa06b8264
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-01-14 21:14:07 +0200

    Declare a new object

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const intersection = (obj1, obj2) => {
+  const intersectionObj = {};
   const firstKeys = Object.keys(obj1);
   for (const attributeName of firstKeys) {
     if (obj1[attributeName] === obj2[attributeName]) {
@@ -11,7 +12,7 @@ const intersection = (obj1, obj2) => {
       delete obj1[attributeName];
     }
   }
-  return obj1;
+  return intersectionObj;
 };
 
 module.exports = intersection;
