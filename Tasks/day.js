1fc99cdb2fe3b684ccfc8c0ab542991c5518ff0f
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-01-14 21:30:18 +0200

    Replace Week with Days

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,7 +5,7 @@ const Days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseWeek = (string) => {
   for (let i = 0; i < Days.length; i++) {
-    if (string.startsWith(Week[i])) return i + 1
+    if (string.startsWith(Days[i])) return i + 1
   }
   return -1;
 };
