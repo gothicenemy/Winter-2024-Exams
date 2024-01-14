19bf26363cb2ad68be66654a342be3dfa3a7e499
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-01-14 21:55:41 +0200

    Replaced forEach on for of, changed naming

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,11 +3,10 @@
 
 const sumNumbers = (obj) => {
   let sum = 0;
-  const keys = Object.keys(obj);
-  keys.forEach((key) => {
-    const value = obj[key];
+  const values = Object.values(obj);
+  for (const value of values) {
     if (typeof value === 'number') sum += value;
-  });
+  }
   return sum;
 };
 
