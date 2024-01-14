8cba91a711923019b91cb602145d66b4b93ce1a1
gothicenemy <maksymovskyi.nazar@lll.kpi.ua>
2024-01-14 21:54:16 +0200

    Deleted unneeded elements

--- Tasks/count.js
+++ Tasks/count.js
@@ -3,15 +3,11 @@
 
 const sumNumbers = (obj) => {
   let sum = 0;
-  {
-  }
   const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
